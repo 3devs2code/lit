9946c90d58d96e1c790d72d2395d903574fb2e87
unknown <bruteforceprodigy.com>
2025-04-24 22:01:59 +0530

    init logic ,folder creation

--- internal/initialise/initialise.go
+++ internal/initialise/initialise.go
@@ -1,8 +1,29 @@
 package initialise
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func InitRepo() {
+func InitRepo() error {
+	if _, err := os.Stat(".lit"); !os.IsNotExist(err) {
+		return fmt.Errorf("directory already exists")
+	}
 
-	fmt.Println("INIT REPO")
+	err := os.MkdirAll(".lit/objects", 0755)
+	if err != nil {
+		return fmt.Errorf("failed to create directory: %v", err)
+	}
+
+	if err := os.MkdirAll(".lit/refs/heads", 0755); err != nil {
+		return fmt.Errorf("failed to create refs/heads directory: %v", err)
+	}
+
+	headContent := []byte("ref: refs/heads/master\n")
+	if err := os.WriteFile(".lit/HEAD", headContent, 0644); err != nil {
+		return fmt.Errorf("failed to create HEAD file: %v", err)
+	}
+
+	fmt.Println("Initialized empty Lit repository in .lit/")
+	return nil
 }
